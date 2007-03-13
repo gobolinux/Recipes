d16d5fed5685152b4d5008288611487c843701ea
Lucas Correia Villa Real <lucasvr@gobolinux.org>
2007-03-13 16:55:30 +0000

    Revised recipe for the Ion Window Manager.

---
+++ trunk/Ion/20070203/Resources/Defaults/Settings/ion3/cfg_ion.lua
@@ -0,0 +1,60 @@
+--
+-- Ion main configuration file
+--
+-- This file only includes some settings that are rather frequently altered.
+-- The rest of the settings are in cfg_ioncore.lua and individual modules'
+-- configuration files (cfg_modulename.lua).
+--
+
+-- Set default modifiers. Alt should usually be mapped to Mod1 on
+-- XFree86-based systems. The flying window keys are probably Mod3
+-- or Mod4; see the output of 'xmodmap'.
+--META="Mod1+"
+--ALTMETA=""
+
+-- Terminal emulator
+--XTERM="xterm"
+
+-- Some basic settings
+ioncore.set{
+    -- Maximum delay between clicks in milliseconds to be considered a
+    -- double click.
+    --dblclick_delay=250,
+
+    -- For keyboard resize, time (in milliseconds) to wait after latest
+    -- key press before automatically leaving resize mode (and doing
+    -- the resize in case of non-opaque move).
+    --kbresize_delay=1500,
+
+    -- Opaque resize?
+    --opaque_resize=false,
+
+    -- Movement commands warp the pointer to frames instead of just
+    -- changing focus. Enabled by default.
+    --warp=true,
+}
+
+-- cfg_ioncore contains configuration of the Ion 'core'
+dopath("cfg_ioncore")
+
+-- Load some kludges to make apps behave better.
+dopath("cfg_kludges")
+
+-- Load some modules. Disable the loading of cfg_modules by commenting out 
+-- the corresponding line with -- if you don't want the whole default set 
+-- (everything except mod_dock). Then uncomment the lines for the modules
+-- you want. 
+dopath("cfg_modules")
+--dopath("mod_query")
+--dopath("mod_menu")
+--dopath("mod_tiling")
+--dopath("mod_statusbar")
+--dopath("mod_dock")
+--dopath("mod_sp")
+
+-- Define some default layouts. This is also done by cfg_modules
+-- if loaded.
+--dopath("cfg_layouts", true)
+
+-- Deprecated.
+dopath("cfg_user", true)
---
+++ trunk/Ion/20070203/Resources/Defaults/Settings/ion3/cfg_pwm.lua
@@ -0,0 +1,96 @@
+--
+-- PWM main configuration file
+--
+-- This file only includes some settings that are rather frequently altered,
+-- and the differences between PWM and Ion. The rest of the settings are in 
+-- cfg_ioncore.lua and individual modules' configuration files 
+-- (cfg_modulename.lua).
+--
+
+-- Set default modifiers. Alt should usually be mapped to Mod1 on
+-- XFree86-based systems. The flying window keys are probably Mod3
+-- or Mod4; see the output of 'xmodmap'.
+--META="Mod1+"
+--ALTMETA=""
+
+-- Some basic settings
+ioncore.set{
+    -- Maximum delay between clicks in milliseconds to be considered a
+    -- double click.
+    --dblclick_delay=250,
+
+    -- For keyboard resize, time (in milliseconds) to wait after latest
+    -- key press before automatically leaving resize mode (and doing
+    -- the resize in case of non-opaque move).
+    --kbresize_delay=1500,
+
+    -- Opaque resize?
+    --opaque_resize=false,
+
+    -- Movement commands warp the pointer to frames instead of just
+    -- changing focus. Enabled by default.
+    --warp=true,
+}
+
+-- cfg_ioncore contains configuration of the Ion 'core'
+dopath("cfg_ioncore")
+
+-- Load some modules. 
+--dopath("cfg_modules")
+--dopath("mod_query")
+dopath("mod_menu")
+--dopath("mod_tiling")
+--dopath("mod_statusbar")
+dopath("mod_dock")
+--dopath("mod_sp")
+
+
+--
+-- PWM customisations to bindings and menus
+--
+
+
+-- Unbind anything using mod_query and rebinding to mod_menu where
+-- applicable.
+
+defbindings("WScreen", {
+    kpress(ALTMETA.."F12", "mod_menu.menu(_, _sub, 'mainmenu', {big=true})"),
+})
+
+defbindings("WMPlex.toplevel", {
+    kpress(ALTMETA.."F1", nil),
+    kpress(META..   "F1", "ioncore.exec_on(_, ':man pwm3')"),
+    kpress(ALTMETA.."F3", nil),
+    kpress(META..   "F3", nil),
+    kpress(ALTMETA.."F4", nil),
+    kpress(ALTMETA.."F5", nil),
+    kpress(ALTMETA.."F6", nil),
+    kpress(ALTMETA.."F9", nil),
+    kpress(META.."G", nil),
+    kpress(META.."A", nil),
+})
+
+defbindings("WFrame", {
+    kpress(META.."M", "mod_menu.menu(_, _sub, 'ctxmenu')"),
+})
+
+-- Make a new main menu with additional workspace menu.
+
+defmenu("mainmenu", {
+    submenu("Programs",      "appmenu"),
+    menuentry("Lock screen", "ioncore.exec_on(_, 'xlock')"),
+    menuentry("Help",        "ioncore.exec_on(_, ':man pwm3')"),
+    submenu("Workspaces",    "wsmenu"),
+    menuentry("New",         "ioncore.create_ws(_)"),
+    submenu("Styles",        "stylemenu"),
+    submenu("Session",       "sessionmenu"),
+})
+
+-- Workspace menu
+defmenu("wsmenu", {
+    menuentry("New",         "ioncore.create_ws(_)"),
+    menuentry("Close",       "WRegion.rqclose(_sub)",
+                             "_sub:WGroupWS"),
+    submenu("List",          "workspacelist"),
+})
+
